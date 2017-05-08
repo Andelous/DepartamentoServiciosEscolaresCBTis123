42f24c580a8fc929e1367f2e5b9ce1492e07bc21
Angel Daniel Alonso Tovar <andelous.aa@gmail.com>
2017-05-08 18:57:52 -0500

    .

--- Logica/Controladores/ControladorGrupos.cs
+++ Logica/Controladores/ControladorGrupos.cs
@@ -20,7 +20,13 @@ namespace DepartamentoServiciosEscolaresCBTis123.Logica.Controladores
         private DAOMaterias daoMaterias { get; set; }
 
         // Controladores
-        private DAODocentes daoDocentes { get; set; }
+        private ControladorDocentes controladorDocentes
+        {
+            get
+            {
+                return ControladorSingleton.controladorDocentes;
+            }
+        }
         private ControladorEstudiantes controladorEstudiantes
         {
             get
@@ -43,9 +49,6 @@ namespace DepartamentoServiciosEscolaresCBTis123.Logica.Controladores
             daoCarreras = new DAOCarreras();
             daoCatedras = new DAOCatedras();
             daoMaterias = new DAOMaterias();
-
-            // Futuros controladores
-            daoDocentes = new DAODocentes();
         }
         
         // Métodos de manipulación del modelo
@@ -116,7 +119,7 @@ namespace DepartamentoServiciosEscolaresCBTis123.Logica.Controladores
             // Realizamos la operación, y damos manejo a las excepciones
             try
             {
-                listaDocentes = daoDocentes.seleccionarDocentes();
+                listaDocentes = controladorDocentes.seleccionarDocentes();
             }
             catch (MySqlException e)
             {
